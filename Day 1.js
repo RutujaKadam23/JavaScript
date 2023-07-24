dce717ef45cb41c9d28f6816cc061ced9af0f0b6
Rutuja Kadam <128777089+RutujaKadam23@users.noreply.github.com>
2023-07-24 17:01:25 +0530

    Add files via upload

---
+++ Day 1.js
@@ -0,0 +1,33 @@
+//Variables in JavaScript
+var N1 = "Rutuja"
+console.log(N1);
+//Constant
+const pi = 3.14
+console.log(pi)
+//90% instead of var ..let is written
+let Name = "   Rutuja   K"
+//To find character in the string at particular index
+console.log(Name[3])
+//To find Length of a variable
+console.log(Name.length)
+//To find last character of the string
+console.log(Name[Name.length-1])
+//trim()
+console.log(Name.trim())
+//toUpperCase()
+console.log(Name.toUpperCase())
+//toLowerCase()
+console.log(Name.toLowerCase())
+//slice
+console.log(Name.slice(0,4))
+console.log(Name.slice(1))
+//typeof operator
+let age=22
+console.log(typeof Name)
+console.log(typeof age)
+//Convert number to string
+age=age+""
+console.log(typeof age)
+//Convert String to number
+Name=+"Name"
+console.log(typeof Name)
